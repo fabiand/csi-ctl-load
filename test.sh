841b03532a5f5741317c819e5c571a463deda5be
Fabian Deutsch <fabiand@fedoraproject.org>
2025-12-10 14:43:11 +0100

    Dedupe replica count
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- test.sh
+++ test.sh
@@ -12,11 +12,12 @@ oc get sc ocs-storagecluster-ceph-rbd-virtualization
 
 cat $MANIFEST
 
+EXPECTED_REPLICA_COUNT=§(oc get -o jsonpath='{.status.replicas}' -f $MANIFEST)
 for N in $(seq 1 $N_MAX);
 do
   echo "# Iteration $N of $N_MAX"
   oc apply -f $MANIFEST
-  oc wait --for jsonpath=.status.readyReplicas=100 -f $MANIFEST --timeout 15m
+  oc wait --for jsonpath=.status.readyReplicas=$EXPECTED_REPLICA_COUNT -f $MANIFEST --timeout 15m
   oc get pods -o NAME | xargs -P 5 -i -- bash -c "oc logs {} | grep -s \"CK FAILED\" && { echo CORRUPTION DETECTED by pod {} ; oc logs {} | sed \"s/^/> /\" ; }" || :
   kubectl patch statefulset csi-ctl-load --patch '{"spec": {"replicas": 0}}'
   oc wait --for jsonpath=.status.replicas=0 -f $MANIFEST --timeout 15m
